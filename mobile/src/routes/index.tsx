b1dea8b58d210d27120fb4b4b11ac6c015ce7392
Icaro Ramos <polosnramos@hotmail.com>
2024-04-09 01:53:40 -0300

    feat: Create linking for deeplink in routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,30 +1,88 @@
-import { useContext } from 'react';
+import { useEffect, useState } from "react";
 
-import { useTheme, Box } from 'native-base';
+import { useTheme, Box } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { useAuth } from '@hooks/useAuth';
+import { useAuth } from "@hooks/useAuth";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
-import { Loading } from '@components/Loading';
+import { Notification } from "@components/Notification";
+import { Loading } from "@components/Loading";
+import {
+  NotificationWillDisplayEvent,
+  OSNotification,
+  OneSignal,
+} from "react-native-onesignal";
+
+const linking = {
+  prefixes: ["com.icapr.ignitegym://", "ignite-gym://"],
+  config: {
+    screens: {
+      signIn: {
+        path: "/signIn",
+      },
+      signUp: {
+        path: "/signUp",
+      },
+      exercise: {
+        path: "/exercise/:exerciseId",
+      },
+      history: {
+        path: "/history",
+      },
+      profile: {
+        path: "/profile",
+      },
+      NotFound: "*",
+    },
+  },
+};
 
 export function Routes() {
   const { colors } = useTheme();
 
+  const [notification, setNotification] = useState<OSNotification>();
+
   const { user, isLoadingUserStorageData } = useAuth();
 
   const theme = DefaultTheme;
+
   theme.colors.background = colors.gray[700];
 
-  if(isLoadingUserStorageData) {
-    return <Loading />
+  useEffect(() => {
+    const handleNotificarion = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault();
+      const response = event.getNotification();
+      setNotification(response);
+    };
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleNotificarion
+    );
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "foregroundWillDisplay",
+        handleNotificarion
+      );
+  }, []);
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
   }
 
   return (
     <Box flex={1} bg="gray.700">
-      <NavigationContainer theme={theme}>
+      <NavigationContainer theme={theme} linking={linking}>
         {user.id ? <AppRoutes /> : <AuthRoutes />}
+        {notification && (
+          <Notification
+            data={notification}
+            onClose={() => {
+              setNotification(undefined);
+            }}
+          />
+        )}
       </NavigationContainer>
     </Box>
   );
