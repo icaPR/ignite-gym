4b8705f7169ac3e5094ab4d5b833efb33383a756
Icaro Ramos <polosnramos@hotmail.com>
2024-04-09 01:30:02 -0300

    feat: Initialize OneSignal and create getLastExercises and handleNotificationClick functions

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,85 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from '@routes/index';
+import { Routes } from "@routes/index";
 
-import { AuthContextProvider } from '@contexts/AuthContext';
+import { AuthContextProvider } from "@contexts/AuthContext";
 
-import { THEME } from './src/theme';
+import { THEME } from "./src/theme";
 
-import { Loading } from '@components/Loading';
+import { Loading } from "@components/Loading";
+
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal";
+
+import { OneSignal_Keys } from "@env";
+
+import { tagUserDate } from "./src/notifications/notificationsTags";
+import { useEffect, useState } from "react";
+import { api } from "@services/api";
+import { HistoryByDayDTO } from "@dtos/HistoryByDayDTO";
+
+OneSignal.initialize(OneSignal_Keys);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  async function getLastExercises() {
+    try {
+      const currentDate = new Date();
+      const sevenDaysAgo = new Date();
+
+      if (currentDate.getDay() !== 1) {
+        return undefined;
+      }
+
+      sevenDaysAgo.setDate(currentDate.getDate() - 7);
+      sevenDaysAgo.setHours(0, 0, 0, 0);
+
+      const response = await api.get(`/history`);
+
+      let total = 0;
+      response.data.forEach((obj: HistoryByDayDTO) => {
+        obj.data.forEach((item) => {
+          const updatedAtDate = new Date(item.created_at.replace(" ", "T"));
+          if (updatedAtDate > sevenDaysAgo) {
+            total++;
+          }
+        });
+      });
+      return total;
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  }
+
+  useEffect(() => {
+    const itemCount = getLastExercises();
+    if (itemCount !== undefined) {
+      tagUserDate(itemCount.toString());
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {};
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick);
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "click",
+        handleNotificationClick
+      );
+  }, []);
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
